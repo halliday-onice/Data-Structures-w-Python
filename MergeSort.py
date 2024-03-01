668f90ebccb94811efed1d4ae5e5474bcfc6d4e0
halliday-onice <jonatashalliday1206@gmail.com>
2024-02-29 23:25:04 -0300

    Merge Sort v1

--- MergeSort.py
+++ MergeSort.py
@@ -2,9 +2,8 @@ import sys
 
 # trying to really learn the merge sort
 
-A = [2,4, 5, 7, 1, 2, 3 , 6]
 
-print(len(A))
+
 
 # p eh a pos mais esquerda
 #q meio
@@ -14,6 +13,10 @@ R = []
 def Merge(vector, p, q, r):
       n1 = q - p + 1 #n1 eh o indice do vetor Left
       n2 = r - p #n2 eh o indice do vetor Right
+      for i in range(n1):
+            print(n1)
 
 
-
+if __name__ == "__main__":
+      A = [2, 4, 5, 7, 1, 2, 3 , 6]
+      Merge(A,0,3,7)
