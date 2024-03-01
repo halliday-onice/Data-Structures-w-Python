cb7330bf73e60904a4ea7897e0a8e8aabb7abb24
Nelson Nascimento <nelsonnascimento@MacBook-Pro-de-Nelson.local>
2024-03-01 17:08:09 -0300

    merge sort

--- MergeSort.py
+++ MergeSort.py
@@ -1,20 +1,21 @@
 import sys
 
+inf = sys.maxsize
 # trying to really learn the merge sort
-
-
-
 
 # p eh a pos mais esquerda
 #q meio
 #r pos mais a direita
 L = []
 R = []
+#The .extend() method allows you to add more items to the end of a list
 def Merge(vector, p, q, r):
       n1 = q - p + 1 #n1 eh o indice do vetor Left
       n2 = r - p #n2 eh o indice do vetor Right
-      for i in range(n1):
-            print(n1)
+      L = [None for i in range(n1 + 1)]
+      
+      print(len(L))
+            
 
 
 if __name__ == "__main__":
