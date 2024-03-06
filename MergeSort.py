a449b94b87db1e3517721954ac6f8f6aa7fe4c8f
halliday-onice <jonatashalliday1206@gmail.com>
2024-03-06 00:39:33 -0300

    MergeSort working

--- MergeSort.py
+++ MergeSort.py
@@ -2,73 +2,78 @@ import sys
 import random
 # Ao longo do script, estou seguindo a notacao do livro
 
-inf = sys.maxsize
+
 # trying to really learn the merge sort
 
 #p eh a pos mais esquerda
 #q meio
 #r pos mais a direita
-
-
 #lista, inicio e fim
-def MergeSort(vector, p = 0, r = None):
+def MergeSort(vector, p, r):
 
-      if r is None:
-            r = len(vector)
-            print(r)
       #Mergesort(vector,inicio, fim)
       #condicao de parada:
       #se ainda tiver um item
-      if (p < r - 1):
+      if (p < r):
             q = (p + r) // 2 # meio recebe (meio + fim)/2
             MergeSort(vector,p,q) #mergesort(lista,inicio, meio)
-            MergeSort(vector,q + 1,r) #mergesort(lista, item seguinte ao meio, fim)
+            MergeSort(vector, q + 1, r) #mergesort(lista, item seguinte ao meio, fim)
             Merge(vector,p, q, r)
+            print("L",vector[p: q + 1], "and", vector[q + 1: r + 1])
+
 
 
 
 #The .extend() method allows you to add more items to the end of a list
 
 def Merge(vector, p, q, r):
-      R = []
-      L = []
-      q = len(vector) // 2 # meio do vetor
-      p = vector[:q] #pos mais esquerda
-      r = vector[q:] # +1 pq estou excluindo a pos do meio 
-      n1 = len(vector[:q])
-      n2 =  len(vector[q:])
+ 
+      n1 = q - p + 1 # +1 pq o q tá dentro do n1
+      n2 =  r - q
 
 
-      print("len p",len(p))
-      print("len r",len(r))
+      L = [0] * n1
+      R = [0] * n2
 
+      for i in range(0, n1):
+            L[i] = vector[p + i]
 
-      L = p.copy()
-      R = r.copy()
+      for j in range(0, n2):
+            R[j] = vector[q + 1 + j]
       
-
-
-      L.insert(len(L),inf)
-      R.insert(len(R), inf)
-
-      print("L", L)
-      print("R", R)
-
-      i = j = 0
-      for k in range(len(p),len(r)): 
-           if (L[i] <= R[j]):
-                 vector[k] = L[i]
-                 i += 1
-           else:
+      print("N1",n1)
+      print("N2",n2)
+      i = 0
+      j  = 0
+      k = p
+      while i < n1 and j < n2:
+            if L[i] <= R[j]:
+                  vector[k] = L[i]
+                  print("L[I]",L[i])
+                  i += 1
+            else:
                   vector[k] = R[j]
+                  print("R[I]", R[j])
                   j += 1
-      print("sorted vec", vector)
-
+            k += 1
+            
+            
+            print("vector", vector[k])
+      #print("i,j", i,j)
+      #preciso copiar algum elemento de L[] restante se houver
+      while i < n1:
+            vector[k] = L[i]
+            i += 1
+            k += 1
+      while j < n2:
+            vector[k] = R[j]
+            j += 1
+            k += 1
 
 if __name__ == "__main__":
       #random_list = random.sample(range(1,1000), 8)
-      A = [744, 591, 491, 452, 413, 775, 245, 141]
-      
+      A = [744, 591, 491, 452, 413, 775, 245]
+      tamA = len(A)
       print("Caso teste:", A)
-      MergeSort(A)
+      MergeSort(A,0,tamA - 1)
       print("Caso teste ordenado",A)
